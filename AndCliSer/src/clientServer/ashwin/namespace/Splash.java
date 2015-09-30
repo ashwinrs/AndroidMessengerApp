62168b689de9c235a6e9f224931642310cefa6a6
Ashwin <Ash@Jarvis.local>
2015-09-30 09:27:27 -0600

    Initial commit

---
+++ AndCliSer/src/clientServer/ashwin/namespace/Splash.java
@@ -0,0 +1,49 @@
+package clientServer.ashwin.namespace;
+
+import android.app.Activity;
+import android.content.Intent;
+//import android.media.MediaPlayer;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class Splash extends Activity{
+
+	MediaPlayer ourSong;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		
+		//sound lines
+		ourSong = MediaPlayer.create(Splash.this, R.raw.hero);
+		ourSong.start();
+		
+		Thread timer = new Thread(){
+			public void run(){
+				try{
+					sleep(1000);
+				}catch(InterruptedException e){
+					e.printStackTrace();
+				}finally{
+					Intent OpenStartingActivity = new Intent("clientServer.ashwin.namespace.ANDCLISERACTIVITY");
+					startActivity(OpenStartingActivity);
+				}
+			}
+		};
+		timer.start();	
+	
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		
+		super.onPause();
+		ourSong.release();
+		finish();
+	}
+	
+	
+
+}
